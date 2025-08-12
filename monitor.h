854bba5158fd1f8305c24efe28c6051058033b05
amruthareddy5 <65708079+amruthareddy5@users.noreply.github.com>
2025-08-12 09:31:09 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,23 +1,9 @@
-#ifndef MONITOR_H
-#define MONITOR_H
-
-enum class VitalStatus {
-  Normal,
-  TemperatureCritical,
-  PulseRateCritical,
-  Spo2Critical
-};
-
-struct VitalLimits {
-  float tempMin = 95.0f;
-  float tempMax = 102.0f;
-  float pulseMin = 60.0f;
-  float pulseMax = 100.0f;
-  float spo2Min = 90.0f;
-};
-
-VitalStatus checkVitals(float temperature, float pulseRate, float spo2, const VitalLimits& limits);
-void blinkAlarm(int times = 6, int intervalSec = 1);
+#pragma once
+#include <string>
+bool isTemperatureOk(float temperature);
+bool isPulseOk(float pulseRate);
+bool isSpO2Ok(float spo2);
 int vitalsOk(float temperature, float pulseRate, float spo2);
+void showAlert(const std::string& message);
+void blinkAlarm(int times = 6);
 
-#endif // MONITOR_H
