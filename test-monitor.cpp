7415d3c9aaf5343ad126cce9bfaee8c0272f6590
amruthareddy5 <65708079+amruthareddy5@users.noreply.github.com>
2025-08-12 09:31:50 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,12 +1,8 @@
+#include <gtest/gtest.h>
 #include "./monitor.h"
-#include <assert.h>
 
-void testVitalsLogic() {
-  VitalLimits limits;
-
-  assert(checkVitals(98.6, 75, 95, limits) == VitalStatus::Normal);
-  assert(checkVitals(104.0, 75, 95, limits) == VitalStatus::TemperatureCritical);
-  assert(checkVitals(98.6, 55, 95, limits) == VitalStatus::PulseRateCritical);
-  assert(checkVitals(98.6, 75, 85, limits) == VitalStatus::Spo2Critical);
+TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
+  ASSERT_FALSE(vitalsOk(99, 102, 70));
+  ASSERT_TRUE(vitalsOk(98.1, 70, 98));
 }
 
